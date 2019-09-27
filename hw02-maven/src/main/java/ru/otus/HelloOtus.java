d29c9c2fe23fab49f638a5f6c60076f2471462d5
Yan <yanteppe@gmail.com>
2019-09-27 11:46:36 +0500

    added use Guava

--- hw02-maven/src/main/java/ru/otus/HelloOtus.java
+++ hw02-maven/src/main/java/ru/otus/HelloOtus.java
@@ -1,5 +1,30 @@
 package ru.otus;
 
-public class HelloOtus {
+import com.google.common.collect.Lists;
+import com.google.common.collect.Ordering;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Home work №2. Maven.
+ * Guava demo class
+ */
+public class HelloOtus {
+    public static void main(String args[]) {
+        ArrayList<Integer> numbers = Lists.newArrayList(2, 6, 7, 1, 0, 3, 10, 4, 8, 5, 9);
+        System.out.println("List: " + numbers);
+        System.out.println("Sorted list: " + new HelloOtus().getSortedNumbers(numbers));
+    }
+
+    /**
+     * Sort integers through Guava
+     *
+     * @param numbers list of numbers
+     * @return sorted list of numbers
+     */
+    private List getSortedNumbers(List numbers) {
+        numbers.sort(Ordering.natural());
+        return numbers;
+    }
 }

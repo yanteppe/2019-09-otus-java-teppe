bf78df0e536efd5779a07e22b8424b68485e404f
Yan <yanteppe@gmail.com>
2019-11-22 01:56:34 +0500

    removing block comments

--- hw06-atm/src/main/java/ru/otus/hw/atm/BanknoteContainer.java
+++ hw06-atm/src/main/java/ru/otus/hw/atm/BanknoteContainer.java
@@ -16,19 +16,6 @@ import static ru.otus.hw.banknotes.Ruble.*;
  * ATM banknote storage container
  */
 class BanknoteContainer {
-//    private SortedMap<Ruble2, List> rublesCase = new TreeMap<>();
-//    private SortedMap<Dollar2, List> dollarsCase = new TreeMap<>();
-//    private List<ru.otus.hw.garbage.Ruble2> rubNominal50 = new ArrayList<>();
-//    private List<ru.otus.hw.garbage.Ruble2> rubNominal100 = new ArrayList<>();
-//    private List<ru.otus.hw.garbage.Ruble2> rubNominal500 = new ArrayList<>();
-//    private List<ru.otus.hw.garbage.Ruble2> rubNominal1000 = new ArrayList<>();
-//    private List<ru.otus.hw.garbage.Ruble2> rubNominal5000 = new ArrayList<>();
-//    private List<ru.otus.hw.garbage.Dollar2> dollarNominal5 = new ArrayList<>();
-//    private List<ru.otus.hw.garbage.Dollar2> dollarNominal10 = new ArrayList<>();
-//    private List<ru.otus.hw.garbage.Dollar2> dollarNominal20 = new ArrayList<>();
-//    private List<ru.otus.hw.garbage.Dollar2> dollarNominal50 = new ArrayList<>();
-//    private List<ru.otus.hw.garbage.Dollar2> dollarNominal100 = new ArrayList<>();
-
     private int rubCounter50;
     private int rubCounter100;
     private int rubCounter500;
@@ -39,8 +26,6 @@ class BanknoteContainer {
     private int dollarCounter20;
     private int dollarCounter50;
     private int dollarCounter100;
-//    private int rubleTotal = 0;
-//    private int dollarTotal = 0;
 
     /**
      * Get banknotes by sum
@@ -100,73 +85,4 @@ class BanknoteContainer {
         data.put("dollars", getDollarTotalData());
         return data;
     }
-
-//    private int getDollarTotalData() {
-//        int dollarTotal = 0;
-//        for (DollarNominal nominalKey : dollarsCase.keySet()) {
-//            switch (nominalKey) {
-//                case DOLLAR_5:
-//                    dollarTotal = dollarTotal + dollarsCase.get(nominalKey).size() * DOLLAR_5.getNominal();
-//                    break;
-//                case DOLLAR_10:
-//                    dollarTotal = dollarTotal + dollarsCase.get(nominalKey).size() * DOLLAR_10.getNominal();
-//                    break;
-//                case DOLLAR_20:
-//                    dollarTotal = dollarTotal + dollarsCase.get(nominalKey).size() * DOLLAR_20.getNominal();
-//                    break;
-//                case DOLLAR_50:
-//                    dollarTotal = dollarTotal + dollarsCase.get(nominalKey).size() * DOLLAR_50.getNominal();
-//                    break;
-//                case DOLLAR_100:
-//                    dollarTotal = dollarTotal + dollarsCase.get(nominalKey).size() * DOLLAR_100.getNominal();
-//                    break;
-//            }
-//        }
-//        return dollarTotal;
-//    }
-
-//    Map getGeneralContainerData() {
-//        for (Ruble2 nominalKey : rublesCase.keySet()) {
-//            switch (nominalKey) {
-//                case RUB_50:
-//                    rubleTotal = rubleTotal + rublesCase.get(nominalKey).size() * RUB_50.getNominal();
-//                    break;
-//                case RUB_100:
-//                    rubleTotal = rubleTotal + rublesCase.get(nominalKey).size() * RUB_100.getNominal();
-//                    break;
-//                case RUB_500:
-//                    rubleTotal = rubleTotal + rublesCase.get(nominalKey).size() * RUB_500.getNominal();
-//                    break;
-//                case RUB_1000:
-//                    rubleTotal = rubleTotal + rublesCase.get(nominalKey).size() * RUB_1000.getNominal();
-//                    break;
-//                case RUB_5000:
-//                    rubleTotal = rubleTotal + rublesCase.get(nominalKey).size() * RUB_5000.getNominal();
-//                    break;
-//            }
-//        }
-//        for (Dollar2 nominalKey : dollarsCase.keySet()) {
-//            switch (nominalKey) {
-//                case DOLLAR_5:
-//                    dollarTotal = dollarTotal + dollarsCase.get(nominalKey).size() * DOLLAR_5.getNominal();
-//                    break;
-//                case DOLLAR_10:
-//                    dollarTotal = dollarTotal + dollarsCase.get(nominalKey).size() * DOLLAR_10.getNominal();
-//                    break;
-//                case DOLLAR_20:
-//                    dollarTotal = dollarTotal + dollarsCase.get(nominalKey).size() * DOLLAR_20.getNominal();
-//                    break;
-//                case DOLLAR_50:
-//                    dollarTotal = dollarTotal + dollarsCase.get(nominalKey).size() * DOLLAR_50.getNominal();
-//                    break;
-//                case DOLLAR_100:
-//                    dollarTotal = dollarTotal + dollarsCase.get(nominalKey).size() * DOLLAR_100.getNominal();
-//                    break;
-//            }
-//        }
-//        Map<String, Integer> data = new HashMap<>();
-//        data.put("rubles", rubleTotal);
-//        data.put("dollars", dollarTotal);
-//        return data;
-//    }
 }

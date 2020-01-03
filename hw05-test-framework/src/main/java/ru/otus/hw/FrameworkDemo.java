29331bdbdcb90ecbb558905e28229095af43a362
Yan <yanteppe@gmail.com>
2020-01-03 18:23:49 +0500

    edit: classpath changed to local variable

--- hw05-test-framework/src/main/java/ru/otus/hw/FrameworkDemo.java
+++ hw05-test-framework/src/main/java/ru/otus/hw/FrameworkDemo.java
@@ -1,10 +1,10 @@
 package ru.otus.hw;
 
 public class FrameworkDemo {
-    private static String classPath = "ru.otus.hw.TestMethods";
 
     public static void main(String[] args) {
         if (args.length == 0) {
+            String classPath = "ru.otus.hw.TestMethods";
             args = new String[]{classPath};
         }
         TestClass testClass = new TestClass(args[0]);

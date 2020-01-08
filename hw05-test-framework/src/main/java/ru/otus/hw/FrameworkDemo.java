d39e968a35fa1acf1325952797287552a41f4e7d
Yan <yanteppe@gmail.com>
2020-01-08 16:08:16 +0500

    edit JavaDoc

--- hw05-test-framework/src/main/java/ru/otus/hw/FrameworkDemo.java
+++ hw05-test-framework/src/main/java/ru/otus/hw/FrameworkDemo.java
@@ -3,7 +3,7 @@ package ru.otus.hw;
 public class FrameworkDemo {
 
     /**
-     * Before starting, in arguments specify the path of the root package of the framework<br>
+     * Before starting, in arguments specify the path of the root package of the test framework<br>
      * ru.otus.hw
      *
      * @param args arguments from startup options (ru.otus.hw)

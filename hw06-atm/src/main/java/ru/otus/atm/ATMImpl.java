30313b4881ab8f95a951b0ecd47e1dbe37067192
Yan <yanteppe@gmail.com>
2020-01-25 02:00:04 +0500

    ATMImpl.java revert changes

--- hw06-atm/src/main/java/ru/otus/atm/ATMImpl.java
+++ hw06-atm/src/main/java/ru/otus/atm/ATMImpl.java
@@ -47,7 +47,7 @@ public class ATMImpl implements ATM {
      */
     public void displayAccountStatus() {
         System.out.println("Account status: " + banknoteContainer.getBanknoteContainerTotalSum() +
-                ", container: " + banknoteContainer.getBanknotesContainer().toString());
+                ", banknotes: " + banknoteContainer.getBanknotesContainer().toString());
     }
 
     /**

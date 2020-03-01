c29426efda9c88ed2892f76d5facd5e0bfef70e2
Yan <yanteppe@gmail.com>
2020-03-01 23:51:50 +0500

    added orm implementation

--- hw10-hibernate/src/main/java/ru/otus/hibernate/orm/dao/UserDaoORM.java
+++ hw10-hibernate/src/main/java/ru/otus/hibernate/orm/dao/UserDaoORM.java
@@ -1,4 +1,30 @@
 package ru.otus.hibernate.orm.dao;
 
-public class UserDaoORM {
+import ru.otus.hibernate.core.dao.UserDao;
+import ru.otus.hibernate.core.model.User;
+import ru.otus.hibernate.core.session_manager.SessionManager;
+
+import java.util.Optional;
+
+public class UserDaoORM implements UserDao {
+
+   @Override
+   public long saveUser(User user) {
+      return 0;
+   }
+
+   @Override
+   public Optional<User> findById(long id) {
+      return Optional.empty();
+   }
+
+   @Override
+   public Optional<User> loadUser(long id) {
+      return Optional.empty();
+   }
+
+   @Override
+   public SessionManager getSessionManager() {
+      return null;
+   }
 }
---
+++ hw10-hibernate/src/main/java/ru/otus/hibernate/orm/helper/HibernateHelper.java
@@ -0,0 +1,4 @@
+package ru.otus.hibernate.orm.helper;
+
+public class HibernateHelper {
+}

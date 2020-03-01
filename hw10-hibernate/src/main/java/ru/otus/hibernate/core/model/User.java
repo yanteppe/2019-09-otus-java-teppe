c7a8d39be467d12bdd85ef391c197736a36f8527
Yan <yanteppe@gmail.com>
2020-03-02 02:22:23 +0500

    minor edits

--- hw10-hibernate/src/main/java/ru/otus/hibernate/core/model/User.java
+++ hw10-hibernate/src/main/java/ru/otus/hibernate/core/model/User.java
@@ -1,72 +1,72 @@
 package ru.otus.hibernate.core.model;
 
-import org.hibernate.annotations.Cascade;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String name;
-    @OneToOne(cascade = CascadeType.ALL)
-    private AddressDataSet address;
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<PhoneDataSet> phones = new ArrayList<>();
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private long id;
+   private String name;
+   @OneToOne(cascade = CascadeType.ALL)
+   private AddressDataSet address;
+   @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+   private List<PhoneDataSet> phones = new ArrayList<>();
 
-    public User() {
-    }
+   public User() {
+   }
 
-    public User(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+   public User(long id, String name) {
+      this.id = id;
+      this.name = name;
+   }
 
-    public User(String name) {
-        this.name = name;
-    }
+   public User(String name) {
+      this.name = name;
+   }
 
-    public long getId() {
-        return id;
-    }
+   public long getId() {
+      return id;
+   }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+   public void setId(long id) {
+      this.id = id;
+   }
 
-    public String getName() {
-        return name;
-    }
+   public String getName() {
+      return name;
+   }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+   public void setName(String name) {
+      this.name = name;
+   }
 
-    public AddressDataSet getAddress() {
-        return address;
-    }
+   public AddressDataSet getAddress() {
+      return address;
+   }
 
-    public void setAddress(AddressDataSet address) {
-        this.address = address;
-    }
+   public void setAddress(AddressDataSet address) {
+      this.address = address;
+   }
 
-    public List<PhoneDataSet> getPhones() {
-        return phones;
-    }
+   public List<PhoneDataSet> getPhones() {
+      return phones;
+   }
 
-    public void setPhones(List<PhoneDataSet> phones) {
-        this.phones = phones;
-    }
+   public void setPhones(List<PhoneDataSet> phones) {
+      this.phones = phones;
+   }
 
-    @Override
-    public String toString() {
-        return "User{" +
-              "id=" + id +
-              ", name='" + name + '\'' +
-              ", address=" + address +
-              ", phones=" + phones +
-              '}';
-    }
+   @Override
+   public String toString() {
+      return "User{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", address=" + address +
+            ", phones=" + phones +
+            '}';
+   }
 }

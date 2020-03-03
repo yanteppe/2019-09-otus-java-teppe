190338b8e672b775014a4844828292790fddc711
Yan <yanteppe@gmail.com>
2020-03-03 21:15:51 +0500

    removing comments

--- hw12-web-server/src/main/java/ru/otus/web_server/Main.java
+++ hw12-web-server/src/main/java/ru/otus/web_server/Main.java
@@ -15,36 +15,8 @@ public class Main {
    private static final int SERVER_PORT = 8989;
    private static final String STATIC_RESOURCES = "/static";
 
-
    public static void main(String[] args) {
       new ORMStarter(HIBERNATE_CONFIG_FILE, annotatedClasses).start();
       new ServerStarter(SERVER_PORT, STATIC_RESOURCES).start();
    }
-
-//   public static void main(String[] args) throws Exception {
-//      String hashLoginServiceConfigPath = FileSystemHelper.localFileNameOrResourceNameToFullPath(HASH_LOGIN_SERVICE_CONFIG_NAME);
-//      var dataSource = new DataSourceH2DB();
-//      SessionManagerJdbc sessionManager = new SessionManagerJdbc(dataSource);
-////      new H2DBInit().initTables(dataSource);
-//      DbExecutor<User> userDbExecutor = new DbExecutor<>();
-//      UserDao userDao = new UserDaoJDBC(sessionManager, userDbExecutor);
-//      DBServiceUser dbServiceUser = new DbServiceUserImpl(userDao);
-//      UserAuthService userAuthServiceForFilterBasedSecurity = new UserAuthService(userDao);
-//      LoginService loginServiceForBasicSecurity = new HashLoginService(REALM_NAME, hashLoginServiceConfigPath);
-////      LoginService loginServiceForBasicSecurity = new InMemoryLoginServiceImpl(userDao);
-//
-//      Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
-//      TemplateProcessor templateProcessor = new TemplateProcessor(TEMPLATES_DIR);
-//
-//      UsersWebServer usersWebServer = new UsersWebServerImpl(WEB_SERVER_PORT,
-//            FILTER_BASED,
-//            userAuthServiceForFilterBasedSecurity,
-//            loginServiceForBasicSecurity,
-//            userDao,
-//            gson,
-//            templateProcessor);
-//
-//      usersWebServer.start();
-//      usersWebServer.join();
-//   }
 }
